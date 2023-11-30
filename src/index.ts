f182416b6dc9a9259ad6a5dad24a27c9e9428684
titsex <aravan962@gmail.com>
2023-11-30 18:03:19 +0300

    start prompt handling.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 #!/usr/bin/env node
 
+import { prompt } from 'enquirer'
 import { readdirSync } from 'fs'
 import { join } from 'path'
 
 const templatesPath = join(__dirname, '..', 'templates')
 const templates = readdirSync(templatesPath)
 
-console.log(templates)
+prompt([
+    {
+        name: 'template',
+        type: 'select',
+        choices: templates,
+        message: 'Select a template',
+    },
+    {
+        name: 'path',
+        type: 'input',
+        message: 'Type a folder name',
+    },
+]).then(console.log) // TODO: copy template to specified path.
